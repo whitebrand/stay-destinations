0c1e6aacfbd9e4d267d2e7cdb6cc597d01e5b0e5
Sebastián García <crechinesco@invbit.es>
2024-02-01 13:46:20 +0100

    fix: missing attribute and wrong type in hook response

--- src/core/usecases/useDestinationsList.test.tsx
+++ src/core/usecases/useDestinationsList.test.tsx
@@ -2,10 +2,11 @@ import { renderHook, waitFor } from '@testing-library/react-native';
 import useDestinationsList from './useDestinationsList';
 import DestinationsGateway from '../infrastructure/gateways/V1/EstablishmentsGateway/DestinationsGateway';
 import QueryHookWrapper from '../../utils/test/QueryHookWrapper';
+import { DestinationEntity } from '../entities/Destination';
 
 describe('useDestinationsList hook', () => {
   it('calls the getDestinations gateway method', async () => {
-    const FAKE_GET_DESTINATIONS_RESPONSE = [{ id: 1, name: 'example', isFeatured: false, hasChildren: false }];
+    const FAKE_GET_DESTINATIONS_RESPONSE: DestinationEntity[] = [{ id: '1', name: 'example', isFeatured: false, hasChildren: false, childs: [] }];
     const mockGetDestinations = jest.fn();
     
     jest.spyOn(DestinationsGateway, 'getDestinations').mockImplementation(mockGetDestinations).mockResolvedValue(FAKE_GET_DESTINATIONS_RESPONSE);
